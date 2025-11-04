f7c78118e125b37488d77d86146e7db4efa044cb
Aaditya <115972745+Aaditya-7937@users.noreply.github.com>
2025-11-04 10:28:42 +0530

    Changes to JS in take_ss.py

--- take_ss.py
+++ take_ss.py
@@ -36,7 +36,7 @@ def extract_and_save_with_id(driver:webdriver.Chrome, land_id:int,  data:pandas.
 
         driver.get(url)
         time.sleep(5)
-        ELEMENTS_TO_HIDE_JS = ELEMENTS_TO_HIDE_JS = """
+        ELEMENTS_TO_HIDE_JS = """
 
     function hideNonMapElements() {
         // Get the map canvas first
