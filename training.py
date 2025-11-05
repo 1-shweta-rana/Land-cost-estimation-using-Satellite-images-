d9415f045fc73f6afb7e5abc0d7880147c24247e
Aaditya <115972745+Aaditya-7937@users.noreply.github.com>
2025-11-05 22:02:44 +0530

    Completed basic training pipeline

--- training.py
+++ training.py
@@ -1,33 +1,50 @@
 import transformers
 import torch
+import xgboost
 from transformers import AutoImageProcessor, AutoModel
 from PIL import Image
 import os
 import pandas as pd
-from utils import preprocess_data
+from utils import preprocess_data, attach_embeddings_to_data
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from xgboost import XGBRegressor
 
 dataset = pd.read_csv('./dataset1.csv')
 
-model_name = 'facebook/dinov2-small'
-print(f"Is GPU Available: {torch.cuda.is_available()}")
+transformer_model_name = 'facebook/dinov2-small'
 save_path = './saved_transformer'
-
-if not os.path.exists('./saved_transformer'):
-    print("Can't find transformer locally...")
-    model = AutoModel.from_pretrained(model_name)
-    processor = AutoImageProcessor.from_pretrained(model_name)
-    
-    model.save_pretrained(save_path)
-    processor.save_pretrained(save_path)
-    print(f"Saved model to {save_path}")
-
-else:
-    print(f"Loading model from {save_path}")
-    model = AutoModel.from_pretrained(save_path)
-    processor = AutoImageProcessor.from_pretrained(save_path)
-    print(f"Successfully loaded model from {save_path}")
-
 image_path = './images'
+
+preprocess_data(dataset)
+attach_embeddings_to_data(image_path, dataset, transformer_model_name, save_path)
+
+
+print(dataset.head())
+
+# Training
+X = dataset.drop(columns=["Price_per_Acre"])
+y = dataset["Price_per_Acre"]
+
+x_train, x_valid, y_train, y_valid = train_test_split(X, y, stratify=y)
+
+hyperparams = {
+    "n_estimators":100,
+    "device":"cuda" if torch.cuda.is_available() else "cpu"
+}
+
+model = XGBRegressor(**hyperparams)
+model.fit(x_train, y_train)
+preds = model.predict(x_valid)
+
+mae = mean_absolute_error(y_valid, preds)
+print(f"{mae=}")
+
+
+
+
+
+
 
 
 
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import json
 import os
-from transformers import AutoModel, AutoImageProcessor
+from transformers import AutoImageProcessor, AutoModel
+from PIL import Image
+import torch
+
 def extract_lat_long_from_data(dataset: pd.DataFrame, land_id:int) -> dict:
 
     row = dataset.loc[dataset["Land_ID"] == land_id, ["Latitude", "Longitude"]]
@@ -39,13 +42,39 @@ def get_model_and_processor(model_name: str, save_path : str):
         processor.save_pretrained(save_path)
         print(f"Saved model to {save_path}")
     else:
+        print(f"Loading model from {save_path}")
         model = AutoModel.from_pretrained(save_path)
         processor = AutoImageProcessor.from_pretrained(save_path)
+        print(f"Successfully loaded model from {save_path}")
 
     return model,processor
 
+def find_and_attach(image_embedding: torch.Tensor, dataset: pd.DataFrame, image) -> None:
 
-def preprocess_data(dataset: pd.DataFrame) -> None:
+    image_id = image.split(".")[0]
+    row = dataset["Land_ID"] == image_id
+    embedding_np = image_embedding.cpu().numpy() if image_embedding.is_cuda else image_embedding.numpy()
+    dataset.loc(row, 'embedding') = [embedding_np]
+
+def attach_embeddings_to_data(image_path: str, dataset: pd.DataFrame, model_name: str, save_path: str) -> None:
+
+    print(f"Is GPU Available: {torch.cuda.is_available()}")
+    model, processor = get_model_and_processor(model_name, save_path)
+    for image in os.listdir(image_path):
+
+        curr_image_path = os.path.join(image_path, image)
+        curr_image = Image.open(curr_image_path)
+        inputs = processor(curr_image, return_tensors = "pt")
+        
+        with torch.no_grad():
+            outputs = model(**inputs)
+        
+        image_embedding = outputs.last_hidden_state[:, 0, ::]
+
+        find_and_attach(image_embedding, dataset, image)
+        print(f"Attached embedding for {image}")
+
+# def preprocess_data(dataset: pd.DataFrame) -> None:
 
     #Cleaning (dropping nulls)
     #remove rows without land_id
